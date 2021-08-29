c22612befa0e2bdbd388c058e6c33a6e6eb03ffe
arash.ph <arash.ph17@gmail.com>
2021-08-29 12:59:42 +0430

    init commit :)

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
